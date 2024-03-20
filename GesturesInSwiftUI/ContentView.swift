1c05a6810b136cfee33132431df160e83c39b189
Theordius <87307483+Theordius@users.noreply.github.com>
2024-03-20 12:03:03 +0100

    Initial Commit

---
+++ GesturesInSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GesturesInSwiftUI
+//
+//  Created by Rafał Gęsior on 20/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GesturesInSwiftUI/GesturesInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesInSwiftUIApp.swift
+//  GesturesInSwiftUI
+//
+//  Created by Rafał Gęsior on 20/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct GesturesInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
